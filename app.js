0fea957844a96b3fb5b3d1b2f6a35464947ac1dc
Ferruh <ferruhcihan20@gmail.com>
2022-02-09 04:42:10 +0300

    feat:update app.js

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const http = require("http");
-
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const path = require("path");
 const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello NodeJS!");
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
 });
 
-server.listen(port, () => {
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+
+app.get("/blog", (req, res) => {
+  res.render("pages/blog");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("pages/contact");
+});
+
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("pages/register");
+});
+
+app.listen(port, () => {
   console.log(`Server started on http://${hostname}:${port}/`);
 });

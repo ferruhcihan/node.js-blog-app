dc12df787a87803c5fc1192627bef4a97b9b6296
Ferruh <ferruhcihan20@gmail.com>
2022-02-11 03:50:36 +0300

    feat:update app.js

--- app.js
+++ app.js
@@ -5,19 +5,32 @@ const path = require("path");
 const hostname = "127.0.0.1";
 const port = 3000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
 mongoose.connect("mongodb://127.0.0.1/nodeblog_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+app.use(fileUpload());
+
 app.use(express.static("public"));
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 const main = require("./routes/main");
+const posts = require("./routes/posts");
+
 app.use("/", main);
+app.use("/posts", posts);
 
 app.listen(port, () => {
   console.log(`Server started on http://${hostname}:${port}/`);

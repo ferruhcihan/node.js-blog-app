f2f5bb4a85c519802fa60f22e6ee25e7714d8721
Ferruh <ferruhcihan20@gmail.com>
2022-02-13 01:03:22 +0300

    feat:update generateDate func

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const moment = require("moment");
+const generateDate = require("./helpers/generateDate").generateDate;
 
 mongoose.connect("mongodb://127.0.0.1/nodeblog_db", {
   useNewUrlParser: true,
@@ -18,15 +18,7 @@ app.use(fileUpload());
 
 app.use(express.static("public"));
 
-const hbs = exphbs.create({
-  helpers: {
-    generateDate(date, format) {
-      return moment(date).format(format);
-    },
-  },
-});
-
-app.engine("handlebars", hbs.engine);
+app.engine("handlebars", exphbs.engine({ helpers: { generateDate } }));
 app.set("view engine", "handlebars");
 
 // parse application/x-www-form-urlencoded

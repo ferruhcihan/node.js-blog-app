74a36c087f627d1e326eee748916993ebe1f4a85
Ferruh <ferruhcihan20@gmail.com>
2022-02-14 01:53:51 +0300

    feat:add display/hide links func

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,11 @@ router.get("/register", (req, res) => {
 
 router.post("/register", (req, res) => {
   User.create(req.body, (error, user) => {
-    res.redirect("/");
+    req.session.sessionFlash = {
+      type: "alert alert-success",
+      message: "User is created successfully",
+    };
+    res.redirect("/users/login");
   });
 });
 
@@ -30,6 +34,12 @@ router.post("/login", (req, res) => {
     } else {
       res.redirect("/users/register");
     }
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
   });
 });
 

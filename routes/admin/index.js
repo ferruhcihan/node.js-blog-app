23eb4ccedacba3991e068968db9c14374faa9ade
Ferruh <ferruhcihan20@gmail.com>
2022-02-16 02:36:14 +0300

    feat:add edit post page and func

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../../models/Category");
+const Post = require("../../models/Post");
+const path = require("path");
 
 router.get("/", (req, res) => {
   res.render("admin/index");
@@ -26,6 +28,54 @@ router.post("/categories", (req, res) => {
 router.delete("/categories/:id", (req, res) => {
   Category.remove({ _id: req.params.id }).then(() => {
     res.redirect("/admin/categories");
+  });
+});
+
+router.get("/posts", (req, res) => {
+  Post.find({})
+    .populate({ path: "category", model: Category })
+    .lean()
+    .sort({ $natural: -1 })
+    .then((posts) => {
+      res.render("admin/posts", { posts: posts });
+    });
+});
+
+router.delete("/posts/:id", (req, res) => {
+  Post.remove({ _id: req.params.id }).then(() => {
+    res.redirect("/admin/posts");
+  });
+});
+
+router.get("/posts/edit/:id", (req, res) => {
+  Post.findOne({ _id: req.params.id })
+    .lean()
+    .then((post) => {
+      Category.find({})
+        .lean()
+        .then((categories) => {
+          res.render("admin/editpost", { post: post, categories: categories });
+        });
+    });
+});
+
+router.put("/posts/:id", (req, res) => {
+  let post_image = req.files.post_image;
+  post_image.mv(
+    path.resolve(__dirname, "../../public/img/postimages", post_image.name)
+  );
+  let post_image_location = `/img/postimages/${post_image.name}`;
+
+  Post.findOne({ _id: req.params.id }).then((post) => {
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post.date = req.body.date;
+    post.category = req.body.category;
+    post.post_image = post_image_location;
+    console.log("post", post);
+    Post.updateOne({ _id: req.params.id }, post, (err, post) => {
+      res.redirect("/admin/posts");
+    });
   });
 });
 
